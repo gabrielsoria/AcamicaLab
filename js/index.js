e6cd0bab127f43651a3faf27b05a47cd711e9ce8
gabriel-fs <gabriel_fs@hotmail.com>
2019-09-11 23:59:02 -0300

    practico completo para ejercicio de bucles y array

--- js/index.js
+++ js/index.js
@@ -1,77 +1,130 @@
+/**
+ * SECCION DE VARIABLES GLOBALES.
+ * DEFINIR AQUI LAS VARIABLES GLOBALES NECESARIAS.
+ */
 
-personas = [];
-identificador = 1;
-personasEncontradas = [];
+
+
+
+
+// 1- HACER UNA FUNCION QUE RECIBE POR PARAMETROS UN ARRAY DE PERSONAS Y UN ID. 
+// ESTA FUNCION DEBE BUSCAR EN EL ARRAY Y RETORNAR LA UBICACION DE LA PERSONA QUE 
+// TIENE COMO id EL ID PASADO POR PARAMETRO.
+
+
+
+// 2- COMPLETAR LA FUNCION agregarPersona(nombre, apellido, edad) PARA QUE 
+// CON LA INFORMACION RECIBIDA CREE UN OBJETO CON LAS SIGUIENTES PROPIEDADES
+// id, nombre, apellido, edad.
+// el id es un identificador unico para cada persona. lo creamos nosotros de manera
+// interna, puede ser un numero, una letra, un string, pero debe ser unico para cada persona.
+// USAR LA FUNCION recargarLista(personas) QUE RECIBE POR PARAMETRO UN ARRAY DE PERSONAS
+// Y SE ENCARGA DE MOSTRARLO EN LA LISTA DE LA PANTALLA.
+
+ /**
+ * funcion que recibe la informacion necesaria para crear una persona y la agrega al listado de personas.
+ * @param {string} nombre 
+ * @param {string} apellido 
+ * @param {number} edad 
+ */
+function agregarPersona(nombre, apellido, edad) {
+
+    alert("Usar la informacion recibida para crear una persona y agregarla a la lista de personas.");
+}
+
+
+// 3- COMPLETAR LA FUNCION eliminarPrimero() PARA QUE ELIMINE LA PRIMERA PERSONA 
+// DE LA LISTA. REFRESCAR LA LISTA QUE SE ESTA VISUALIZANDO.
+// USAR LA FUNCION recargarLista(personas) QUE RECIBE POR PARAMETRO UN ARRAY DE PERSONAS
+// Y SE ENCARGA DE MOSTRARLO EN LA LISTA DE LA PANTALLA.
+
+/**
+ * elimina la primera persona de la lista
+ */
+function eliminarPrimero() {
+
+    alert("Hacer funcionalidad para eliminar la primera persona de la lista.");
+}
+
+// 4- COMPLETAR LA FUNCION eliminarUltimo() PARA QUE ELIMINE LA ULTIMA PERSONA 
+// DE LA LISTA. REFRESCAR LA LISTA QUE SE ESTA VISUALIZANDO.
+// USAR LA FUNCION recargarLista(personas) QUE RECIBE POR PARAMETRO UN ARRAY DE PERSONAS
+// Y SE ENCARGA DE MOSTRARLO EN LA LISTA DE LA PANTALLA.
+
+/**
+ * elimina la ultima persona de la lista.
+ */
+function eliminarUltimo() {
+
+    alert("Hacer funcionalidad para eliminar la ultima persona de la lista.");
+}
+
+// 5- COMPLETAR LA FUNCION buscarPersonaPorNombre(nombre) PARA QUE QUITE DEL LISTADO
+// LOS QUE NO COINCIDEN CON EL CRITERIO RECIBIDO.
+// EJ. SI EN EL CRITERIO PUSIERON gab EN LA LISTA DEBERIAN QUEDAR NOMBRES COMO gabriel, gaby
+// PERO NO raul, javier.
+// USAR LA FUNCION string.search(criterio) QUE DEVUELVE LA POSICION DONDE SE ENCUENTRA EL CRITERIO
+// -1 SI NO CONTIENE EL CRITERIO.
+// https://www.w3schools.com/jsref/jsref_search.asp
+// USAR LA FUNCION recargarLista(personas) QUE RECIBE POR PARAMETRO UN ARRAY DE PERSONAS
+// Y SE ENCARGA DE MOSTRARLO EN LA LISTA DE LA PANTALLA
+
+/**
+ * del total de personas cargadas, filtra las que no coinciden con el criterio de busqueda.
+ * @param {string} nombre 
+ */
+function buscarPersonaPorNombre(nombre) {
+
+    alert("Hacer funcionalidad para filtrar las personas que no coincidan con el criterio de busqueda.");
+
+}
+
+
+// 6- COMPLETAR LA FUNCION limpiarBusquedaPersona() PARA QUE VUELVA A MOSTRAR EL LISTADO COMPLETO
+// DE PERSONAS.
+// USAR LA FUNCION recargarLista(personas) QUE RECIBE POR PARAMETRO UN ARRAY DE PERSONAS
+// Y SE ENCARGA DE MOSTRARLO EN LA LISTA DE LA PANTALLA
 
 /**
  * limpia la busqueda de persona.
  */
 function limpiarBusquedaPersona() {
 
-    recargarLista(personas);
+    alert("Hacer funcionalidad para mostrar la lista completa de personas cargadas.");
 }
 
-/**
- * 
- * @param {string} nombre 
- */
-function buscarPersonaPorNombre(nombre) {
 
-    personasEncontradas = [];
-    for(var i = 0; i < personas.length; i++) {
 
-        if(personas[i].nombre.toLowerCase().search(nombre.toLowerCase()) > -1) {
-            personasEncontradas.push(personas[i]);
-        }
-    }
 
-    recargarLista(personasEncontradas);
 
-}
+// 7- COMPLETAR LA FUNCION editarPersona(id) PARA QUE ENCUENTRA LA PERSONA CON EL ID
+// QUE RECIBE POR PARAMETRO, Y SE LA PASE A LA FUNCION abrirActualizacionPersona(persona)
+// DICHA FUNCION SE VA A ENCARGAR DE ABRIR EL FORMULARIO PARA LA EDICION.
 
 /**
- * elimina el primer registro
- */
-function eliminarPrimero() {
-
-    if(personas.length == 0) {
-        return;
-    }
-
-    personas.shift();
-
-    recargarLista(personas);
-}
-
-/**
- * elimina el ultimo registro
- */
-function eliminarUltimo() {
-
-    if(personas.length == 0) {
-        return;
-    }
-
-    personas.pop();
-
-    recargarLista(personas);
-}
-
-/**
- * abre una persona para editarla
+ * recibe un id de persona, debe buscar esa persona en el listado de personas
+ * y abrir el formulario de edicion.
  * @param {number} id 
  */
 function editarPersona(id) {
 
-    var index = buscarPorId(personas, id);
+    alert("Hacer funcionalidad para abrir la edicion de una persona.");
 
-    var persona = personas[index];
-
-    abrirActualizacionPersona(persona);
+    // UNA VEZ ENCONTRADA LA PERSONA, DESCOMENTAR LA SIGUIENTE LINEA Y PASARLE LA PERSONA 
+    // ENCONTRADA, DE MODO QUE SE ABRA LA VENTANA DE EDICION.
+    // abrirActualizacionPersona(persona);
 }
 
+
+
+// 8- COMPLETAR LA FUNCION actualizarPersona(id, nombre, apellido, edad) 
+// PARA QUE ENCUENTRA LA PERSONA CON EL ID QUE RECIBE POR PARAMETRO, Y ACTUALICE SUS 
+// PROPIEDADES.
+// USAR LA FUNCION recargarLista(personas) QUE RECIBE POR PARAMETRO UN ARRAY DE PERSONAS
+// Y SE ENCARGA DE MOSTRARLO EN LA LISTA DE LA PANTALLA
+
 /**
- * Funcion que recibe la persona agregada por pantalla.
+ * funcion que recibe la informacion necesaria para actualizar una persona.
  * @param {number} id 
  * @param {string} nombre 
  * @param {string} apellido 
@@ -79,75 +132,44 @@ function editarPersona(id) {
  */
 function actualizarPersona(id, nombre, apellido, edad) {
 
-    var indexPersona = buscarPorId(personas, id);
-
-    let personaActualizada = personas[indexPersona];
-
-    personaActualizada.nombre = nombre;
-    personaActualizada.apellido = apellido;
-    personaActualizada.edad = edad;
-
-    recargarLista(personas);
+    alert("Usar la informacion recibida para actualizar una persona.");
 }
 
-/**
- * Funcion que recibe la persona agregada por pantalla.
- * @param {string} nombre 
- * @param {string} apellido 
- * @param {number} edad 
- */
-function agregarPersona(nombre, apellido, edad) {
 
-    let nuevaPersona = {
-        id: identificador ++,
-        nombre: nombre,
-        apellido: apellido,
-        edad: edad
-    }
-
-    personas.push(nuevaPersona);
-
-    recargarLista(personas);
-}
+// 9- COMPLETAR LA FUNCION eliminarPersona(id) 
+// PARA QUE ENCUENTRA LA PERSONA CON EL ID QUE RECIBE POR PARAMETRO, Y LA QUITE DE LA LISTA.
+// para eliminar un item de un array podemos usar la funcion 
+// array.splice(indiceItemAEliminar, 1)
+// USAR LA FUNCION recargarLista(personas) QUE RECIBE POR PARAMETRO UN ARRAY DE PERSONAS
+// Y SE ENCARGA DE MOSTRARLO EN LA LISTA DE LA PANTALLA
 
 /**
  * indica que persona hay que eliminar 
  * pasando por parametro el id.
  * @param {number} idPersona 
  */
-function eliminarPersona(idPersona) {
+function eliminarPersona(id) {
 
     
-    var index = buscarPorId(personas, idPersona);
-
-    if(index < 0)
-        return;
-
-    personas.splice(index, 1);
-
-    recargarLista(personas);
+    alert("Hacer la funcionalidad necesaria para eliminar la persona que tiene el id recibido.");
     
 }
 
-/**
- * busca en el array de personas y retorna
- * el index del encontrado.
- * @param {Array} array 
- * @param {number} idPersona 
- */
-function buscarPorId(array, idPersona) {
 
-    var encontrado = -1;
-    for(var i = 0; i < array.length; i++) {
 
-        if(array[i].id == idPersona) {
-            encontrado = i;
-            break;
-        }
-    } 
 
-    return encontrado;
-}
+
+
+
+
+
+/********************************************************************************************* */
+// ////////// LAS SIGUIENTES FUNCIONES SON PARA COMUNICAR EL DOM CON EL JS. /////////////////////
+// //////////////////////////// NO DEBEN SER MODIFICADAS. ///////////////////////////////////////
+/********************************************************************************************* */
+
+
+
 
 
 /**
@@ -250,7 +272,9 @@ function isNuevaPersonaValid() {
     return true;
 }
 
-
+/**
+ * funcion de validacion del formulario de actualizacion.
+ */
 function validarActualizacionPersona() {
 
     // formulario de validacion
